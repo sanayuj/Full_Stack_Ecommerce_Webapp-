5fc6c4cb4f4bbb73a50c88be6d5ab655ef188b49
Sanay UJ <admin@Sanay-MacBook.local>
2022-12-09 19:58:04 +0530

    Updated Database

--- helpers/admin-helpers.js
+++ helpers/admin-helpers.js
@@ -3,45 +3,84 @@ const collections = require('../config/collections');
 const bcrypt = require('bcrypt');
 const { Db, ObjectId } = require("mongodb");
 const { response } = require('express');
+let objectId = require('mongodb').ObjectId
 
-module.exports={
-    doLogin: (data) => {
-        console.log(data,"hahaha");
+module.exports = {
+    doSignup: (data) => {
+        console.log("hihihihi");
         return new Promise(async (resolve, reject) => {
-            var loginStatus=false
-            var response={}
+            data.password = await bcrypt.hash(data.password, 10)
+            db.get().collection(collections.USER_COLLECTION).insertOne(data).then((response) => {
+                resolve(response.insertedId);
+            })
+        })
+
+    },
+    doLogin: (data) => {
+        console.log(data, "hahaha");
+        return new Promise(async (resolve, reject) => {
+            var loginStatus = false
+            var response = {}
             const admin = await db.get().collection(collections.ADMIN_COLLECTION).findOne({ Email: data.Email });
             // console.log(admin);
 
             if (admin) {
-                bcrypt.compare(data.Password, admin.Password).then((result)=>{
-                    // console.log(result,"HAHAHA");
-                    if(result){
+                bcrypt.compare(data.Password, admin.Password).then((result) => {
+                    console.log(result, "HAHAHA");
+                    if (result) {
                         console.log("Loggin sucess");
-                        response.admin=admin 
-                        response.status=true
+                        response.admin = admin
+                        response.status = true
                         resolve(response)
-                    }else{
-                        // console.log("loggin failed");
-                        resolve ({status: false})
+                    } else {
+                        console.log("loggin failed");
+                        resolve({ status: false })
                     }
                 })
-            }else{
-                // console.log("Error Admin")
-                resolve({status: false})
+            } else {
+                console.log("Error Admin")
+                resolve({ status: false })
             }
-                
-            
+
+
         })
     },
-    getuserDetails:()=>{
-        return new Promise(async(resolve,reject)=>{
-await db.get().collection(collections.ADMIN_COLLECTION).find({},function(err,result){
-    if (err) throw err;
-    console.log(result.name);
-    db.close();
-})
+    getuserDetails: () => {
+        return new Promise(async (resolve, reject) => {
+            let details = await db.get().collection(collections.USER_COLLECTION).find({}).toArray()
+            resolve(details)
 
         }
+        )
+    },
+    deleteUser: (userId) => {
+        return new Promise(async (resolve, reject) => {
+            db.get().collection(collections.USER_COLLECTION).deleteOne({ _id: objectId(userId) }).then((response) => {
+                console.log(response)
+                resolve(response);
+            })
+
+
+        })
+    },
+    updateUser: (userId) => {
+        return new Promise(async (resolve, reject) => {
+            db.get().collection(collections.USER_COLLECTION).findOne({ _id: objectId(userId) }).then((details) => {
+                resolve(details)
+            })
+        })
+    },
+    UpdateDetails:(userData)=>{
+        return new Promise(async (resolve, reject) => {
+            db.get().collection(collections.USER_COLLECTION).updateOne({_id:objectId(userData.userId)},{
+                $set:{
+                    Name:userData.Name,
+                    Email:userData.Email
+                }
+            }).then((response)=>{
+            resolve()
+            })
+        }
+    
     )}
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { response } = require('../app');
 const adminHelpers = require('../helpers/admin-helpers');
 const { doLogin } = require('../Register/Register');
 const router = express.Router();
@@ -11,7 +12,9 @@ router.get('/', function(req, res, next)
   if(req.session.adminloggedIn){
     res.redirect('/admin/adminpage')
   }else{
-    res.render('adminLogin');
+   
+    res.render('adminLogin',{"loginErr":req.session.adminlogginErr});
+    req.session.adminlogginErr=false
   }
   
 });
@@ -19,8 +22,11 @@ router.get('/', function(req, res, next)
 
 
 router.get('/adminpage',function(req,res,next){
-  adminHelpers.getuserDetails()
-  res.render('adminpage',{admin:req.session.admin})
+  adminHelpers.getuserDetails().then((details)=>{
+    console.log(details,"haaha");
+    res.render('adminpage',{admin:req.session.admin,details})
+  })
+  
 })
 
 
@@ -33,6 +39,7 @@ router.post('/adminpage',function(req,res,next){
       req.session.admin=response.admin
       res.redirect('/admin/adminpage');
     }else{
+      req.session.adminlogginErr=true
       res.redirect('/admin')
     }
   
@@ -45,5 +52,46 @@ router.get('/logout',(req,res)=>{
   res.redirect('/admin')
 })
 
+router.get('/delete-user/:Id',(req,res)=>{
+  var userId=req.params.Id
+  console.log(userId)
+  adminHelpers.deleteUser(userId).then((response)=>{
+    res.redirect('/admin/adminpage')
+
+  })
+
+})
+
+router.get('/adduser', function(req, res, next) {
+  // adminHelpers.doSignup(req.body)
+  res.render('addUserAdmin');
+
+});
+
+router.post('/addUserAdmin', function(req, res, next) {
+  adminHelpers.doSignup(req.body).then((response)=>{
+    res.redirect('/admin/adminpage');
+  })
+  
+
+});
+
+router.get('/edit-user/:id',(req,res,next)=>{
+  console.log(req.params.id,("gfg"));
+  adminHelpers.updateUser(req.params.id).then((details)=>{
+    // res.redirect('')
+    console.log(details,("xcfxzfzx"));
+  res.render('admin-Editpage',{details})
+  })
+  
+  // console.log("How are u");
+})
+
+router.post('/edit-user',(req,res,next)=>{
+  console.log("ffffffo");
+  adminHelpers.UpdateDetails(req.body).then(()=>{
+    res.redirect("/admin/adminpage")
+  })
+})
 
 module.exports = router;
